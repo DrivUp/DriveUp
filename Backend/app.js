811cc53f6fe1eb4986a357364eeee3075ac42b96
Priyam Pandey <priyampabdey75310@gmail.com>
2025-04-10 22:44:01 +0530

    user login and register created

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectToDb from "./db/db.js";
+import userRoutes from "./routes/user.routes.js";
+
+
 
 dotenv.config();
-
 connectToDb();
+
 const app = express();
+app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));  
+app.use('/users', userRoutes);
 
 
 app.get('/', (req, res) => {
